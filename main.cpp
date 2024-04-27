7874ab229b9caf20eb530e2e4a143da7d734ab38
jAnubhav <jainanubhav2003@gmail.com>
2024-04-27 18:31:44 +0530

    Updated main.cpp and bank.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
-// #include "bin/inc/user.hpp"
-#include "bin/inc/format.hpp"
+#include "bin/inc/user.hpp"
 
 #include <iostream>
 
 int main()
 {
-    // makeLine('=', 10, 20, 30);
-    makeLine('-', 10, 20, 30);
-    printLine('|', "Hello", 10, "World", 20);
-    // printLine("HELLO", 10, "World", 20);
-    // int choice;
-    // accounts::AccountHolder holder;
+    int choice;
+    accounts::AccountHolder holder;
 
-    // while ((choice = initializer()) && choice % 3)
-    //     holder = interactions.at(choice)();
+    while ((choice = initializer()) && choice % 3)
+        holder = interactions.at(choice)();
 
-    // std::cout << "Quiting...";
-    // return 0;
+    std::cout << "Quiting...";
+    return 0;
 }
