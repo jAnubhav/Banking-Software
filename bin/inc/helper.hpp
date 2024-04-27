0e68d0e5913c8a1151d613ad6f504a5be7117531
jAnubhav <jainanubhav2003@gmail.com>
2024-04-27 10:54:10 +0530

    Updated files

---
+++ bin/inc/helper.hpp
@@ -0,0 +1,45 @@
+#ifndef HELPER
+#define HELPER
+
+#include <fstream>
+
+#include <algorithm>
+
+#include <string>
+#include <vector>
+
+namespace functions
+{
+    inline auto str_only(const std::string &str) { return str; }
+
+    inline auto str_int(const std::string &str) { return std::make_pair(str, str.length()); }
+
+    inline auto str_str(const std::string &str) { return std::make_pair(str, str.substr(0, 3)); }
+
+    inline auto str_fir(const std::pair<std::string, std::string> &pair) { return pair.first; }
+}
+
+template <typename T>
+std::vector<T> extractData(std::string file_name, T (*func)(const std::string &))
+{
+    std::string tempStr;
+    std::vector<T> tempVec;
+
+    std::ifstream file("data/" + file_name + ".bin", std::ios_base::binary);
+
+    while (std::getline(file, tempStr))
+        tempVec.push_back(func(tempStr));
+
+    return tempVec;
+}
+
+template <typename T>
+int findLength(const std::vector<T> &vec, std::string (*func)(const T &))
+{
+    size_t len = 0;
+    for (const auto &val : vec)
+        len = std::max(len, func(val).length());
+    return len;
+}
+
+#endif
