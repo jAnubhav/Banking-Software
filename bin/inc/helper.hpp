af75f9987114dde630ba0bc5ad321c28f25a59f8
jAnubhav <jainanubhav2003@gmail.com>
2024-04-28 22:15:00 +0530

    Updated files helper.hpp and helper.cpp

--- bin/inc/helper.hpp
+++ bin/inc/helper.hpp
@@ -3,20 +3,15 @@
 
 #include <fstream>
 
-#include <algorithm>
-
 #include <string>
 #include <vector>
 
-namespace functions
+namespace funcs
 {
-    inline auto str_only(const std::string &str) { return str; }
-
-    inline auto str_int(const std::string &str) { return std::make_pair(str, static_cast<int>(str.length())); }
-
-    inline auto str_str(const std::string &str) { return std::make_pair(str, str.substr(0, 3)); }
-
-    inline auto str_fir(const std::pair<std::string, std::string> &pair) { return pair.first; }
+    inline auto str_int(const std::string &),
+        str_onl(const std::string &),
+        str_str(const std::string &),
+        rChar(const char &, const int &);
 }
 
 template <typename T>
@@ -33,13 +28,8 @@ std::vector<T> extractData(std::string file_name, T (*func)(const std::string &)
     return tempVec;
 }
 
-template <typename T>
-int findLength(const std::vector<T> &vec, std::string (*func)(const T &))
-{
-    size_t len = 0;
-    for (const auto &val : vec)
-        len = std::max(len, func(val).length());
-    return len;
-}
+int findLength(const std::vector<std::string> &);
+
+void printLine(const char &, const char &, const int &, const std::vector<std::string> &, const std::vector<int> &);
 
 #endif
--- bin/lib/helper.cpp
+++ bin/lib/helper.cpp
@@ -3,12 +3,21 @@
 #include <iostream>
 #include <iomanip>
 
-inline auto funcs::str_fir(const std::pair<std::string, std::string> &pair) { return pair.first; }
+#include <algorithm>
+
 inline auto funcs::str_int(const std::string &str) { return std::make_pair(str, str.length()); }
 inline auto funcs::str_onl(const std::string &str) { return str; }
 inline auto funcs::str_str(const std::string &str) { return std::make_pair(str, str.substr(0, 3)); }
 
 inline auto funcs::rChar(const char &ch, const int &len) { return std::string(len, ch); }
+
+int findLength(const std::vector<std::string> &vec)
+{
+    size_t len = 0;
+    for (const auto &str : vec)
+        len = std::max(len, str.length());
+    return len;
+}
 
 void printLine(const char &ch1, const char &ch2, const int &pad, const std::vector<std::string> &val, const std::vector<int> &len)
 {
